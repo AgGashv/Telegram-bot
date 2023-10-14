5080019a13007b36415cbf5b19342f53c3c5c208
GashimovA <agasif15@mail.ru>
2023-10-15 01:50:56 +0300

    'text' variable added: variable stores data about commands

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -6,3 +6,10 @@ def set_default_commands(bot):
     bot.set_my_commands(
         [BotCommand(*i) for i in DEFAULT_COMMANDS]
     )
+
+
+text = ''
+for element in DEFAULT_COMMANDS:
+    text += '/' + ' - '.join(element) + '\n'
+
+
