07b48ea91c4d1ccf74c09bcf0e56a6c7dd1b8710
GashimovA <agasif15@mail.ru>
2023-10-24 21:49:43 +0300

    added new class FlightInfoStateHigh with parent FlightInfoStateLow. FlightInfoStateLow renamed from FlightInfoState

--- handlers/custom_handlers/low.py
+++ handlers/custom_handlers/low.py
@@ -1,8 +1,8 @@
 from telebot import StateMemoryStorage
-from keyboards.inline.low_continue_ask import ask_to_continue
+from keyboards.inline.low_continue_ask import ask_to_continue_low
 from loader import bot
-from states.flight_info import FlightInfoState
-from config_data.config import querystring, direct_url, headers, prices_url
+from states.flight_info import FlightInfoStateLow
+from config_data.config import querystring1, direct_url, headers, prices_url
 from database.database import Cities
 import requests
 import json
@@ -10,25 +10,25 @@ import json
 
 @bot.message_handler(commands=['low'])
 def low(message):
-    bot.set_state(message.chat.id, FlightInfoState.origin_city, message.chat.id)
+    bot.set_state(message.chat.id, FlightInfoStateLow.origin_city, message.chat.id)
     bot.send_message(message.chat.id, "Введите город отправления.")
 
 
-@bot.message_handler(state=FlightInfoState.origin_city)
+@bot.message_handler(state=FlightInfoStateLow.origin_city)
 def get_origin_city(message):
     try:
         Cities.get(Cities.name == message.text.title())
     except Exception:
         bot.send_message(message.from_user.id, "Неправильный ввод. Город не найден.")
     else:
-        bot.set_state(message.from_user.id, FlightInfoState.destination_city, message.chat.id)
+        bot.set_state(message.from_user.id, FlightInfoStateLow.destination_city, message.chat.id)
         bot.send_message(message.from_user.id, "Введите город прибытия.")
 
         with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
             data['origin_city'] = message.text.title()
 
 
-@bot.message_handler(state=FlightInfoState.destination_city)
+@bot.message_handler(state=FlightInfoStateLow.destination_city)
 def get_destination_city(message):
     try:
         Cities.get(Cities.name == message.text.title())
@@ -41,11 +41,11 @@ def get_destination_city(message):
         id_origin = Cities.get(Cities.name == data['origin_city'])
         id_destination = Cities.get(Cities.name == data['destination_city'])
 
-        querystring.update({"origin": Cities[id_origin].code,
+        querystring1.update({"origin": Cities[id_origin].code,
                             "destination": Cities[id_destination].code})
 
         response = requests.request("GET", direct_url, headers=headers,
-                                    params=querystring, timeout=10)
+                                    params=querystring1, timeout=20)
 
         if response.status_code == requests.codes.ok:
             direct_data = json.loads(response.text)
@@ -60,7 +60,7 @@ def get_destination_city(message):
                                                      "destination": Cities[id_destination].code,
                                                      "departure_date": direct_data_date,
                                                      "calendar_type": "departure_date"},
-                                             timeout=10)
+                                             timeout=20)
 
                 if response1.status_code == requests.codes.ok:
                     price_data = json.loads(response1.text)
@@ -86,6 +86,6 @@ def get_destination_city(message):
             bot.send_message(message.from_user.id, "Что-то пошло не по плану. Повторите попытку позже. "
                                                    "Извините за доставленные неудобства.")
 
-        bot.send_message(message.from_user.id, "Хотите продолжить?", reply_markup=ask_to_continue())
+        bot.send_message(message.from_user.id, "Хотите продолжить?", reply_markup=ask_to_continue_low())
 
         bot.set_state(message.from_user.id, StateMemoryStorage(), message.chat.id)
--- states/flight_info.py
+++ states/flight_info.py
@@ -1,7 +1,44 @@
 from telebot.handler_backends import State, StatesGroup
 
 
-class FlightInfoState(StatesGroup):
-    origin_city = State()
-    destination_city = State()
-    flight_info = State()
+class FlightInfoStateLow(StatesGroup):
+    def __init__(self, origin_city=State(), destination_city=State()):
+        self.__origin_city = origin_city
+        self.__destination_city = destination_city
+
+    @property
+    def origin_city(self):
+        return self.__origin_city
+
+    @origin_city.setter
+    def origin_city(self, origin_city):
+        self.__origin_city = origin_city
+
+    @property
+    def destination_city(self):
+        return self.__destination_city
+
+    @destination_city.setter
+    def destination_city(self, destination_city):
+        self.__destination_city = destination_city
+
+
+class FlightInfoStateHigh(FlightInfoStateLow):
+    def __init__(self, origin_city=State(), destination_city=State()):
+        super().__init__(origin_city, destination_city)
+
+    @property
+    def origin_city(self):
+        return self.__origin_city
+
+    @origin_city.setter
+    def origin_city(self, origin_city):
+        self.__origin_city = origin_city
+
+    @property
+    def destination_city(self):
+        return self.__destination_city
+
+    @destination_city.setter
+    def destination_city(self, destination_city):
+        self.__destination_city = destination_city
