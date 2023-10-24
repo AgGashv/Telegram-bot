9da4b340d7409641b2d31bf66bc772a9c59d35dc
GashimovA <agasif15@mail.ru>
2023-10-24 21:46:37 +0300

    renamed from low_callback.py

--- handlers/custom_handlers/callback.py
+++ handlers/custom_handlers/callback.py
@@ -1,13 +1,16 @@
 from loader import bot
 from handlers.custom_handlers.low import low
-from telebot import StateMemoryStorage, State
+from handlers.custom_handlers.high import high
+from telebot import StateMemoryStorage
 from utils import set_bot_commands
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def low_callback(call):
-    if call.data == 'yes':
+    if call.data == 'yes_low':
         low(call.message)
+    elif call.data == 'yes_high':
+        high(call.message)
     elif call.data == 'no':
         bot.set_state(call.message.from_user.id, StateMemoryStorage(), call.message.chat.id)
         bot.send_message(call.message.chat.id, f'Выберите команду:\n{set_bot_commands.text}')
