35733c998e4f444b143aeb5208031f2890a24e13
GashimovA <agasif15@mail.ru>
2023-11-01 22:27:39 +0300

    one_time_keyboard removed

--- keyboards/reply/one_way.py
+++ keyboards/reply/one_way.py
@@ -2,6 +2,6 @@ from telebot.types import ReplyKeyboardMarkup
 
 
 def ask_to_one_way_attribute():
-    markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    markup = ReplyKeyboardMarkup(resize_keyboard=True)
     markup.add('В одну сторону', 'Туда-обратно')
     return markup
