15dd2d8ec9e479c4d625add1cb82dcf654f90082
GashimovA <agasif15@mail.ru>
2023-11-01 22:30:21 +0300

    added check for request and work with database

--- handlers/custom_handlers/low.py
+++ handlers/custom_handlers/low.py
@@ -1,5 +1,7 @@
+from telebot import StateMemoryStorage
 from keyboards.inline.low_continue_ask import ask_to_continue_low
 from keyboards.inline.aviasales_site import site_button
+from database.database import Users
 from loader import bot
 from states.flight_info import FlightInfoStateLow
 from config_data.config import querystring1, direct_url, headers, prices_url
@@ -42,7 +44,7 @@ def get_destination_city(message):
         id_destination = Cities.get(Cities.name == data['destination_city'])
 
         querystring1.update({"origin": Cities[id_origin].code,
-                            "destination": Cities[id_destination].code})
+                             "destination": Cities[id_destination].code})
 
         response = requests.request("GET", direct_url, headers=headers,
                                     params=querystring1, timeout=20)
@@ -64,25 +66,61 @@ def get_destination_city(message):
 
                 if response1.status_code == requests.codes.ok:
                     price_data = json.loads(response1.text)
-                    ticket_info = '{}{} <b>→</b> {}{}\n\n' \
-                                  'Цена: {:,} руб.\n' \
-                                  'Дата: {}\n' \
-                                  'Авиакомпания: {}\n' \
-                                  'Номер рейса: {}'.format(
-                                    Cities[id_origin].flag,
-                                    Cities[id_origin].name,
-                                    Cities[id_destination].name,
-                                    Cities[id_destination].flag,
-                                    direct_data_value,
-                                    direct_data_date[-2:] + direct_data_date[4:8] + direct_data_date[:4],
-                                    price_data['data'].get(direct_data_date).get('airline'),
-                                    price_data['data'].get(direct_data_date).get('flight_number')
-                                    ).replace(',', ' ')
+                    if price_data['data'] == {}:
+                        ticket_info = '{}{} <b>→</b> {}{}\n\n' \
+                                      'Цена: {:,} руб.\n' \
+                                      'Дата: {}\n' \
+                                      'Информация об авиакомпании и номере рейса отсутствуют.'.format(
+                                        Cities[id_origin].flag,
+                                        Cities[id_origin].name,
+                                        Cities[id_destination].name,
+                                        Cities[id_destination].flag,
+                                        direct_data_value,
+                                        direct_data_date[-2:] + direct_data_date[4:8] + direct_data_date[:4],
+                                        ).replace(',', ' ')
+                    else:
+                        ticket_info = '{}{} <b>→</b> {}{}\n\n' \
+                                      'Цена: {:,} руб.\n' \
+                                      'Дата: {}\n' \
+                                      'Авиакомпания: {}\n' \
+                                      'Номер рейса: {}'.format(
+                                        Cities[id_origin].flag,
+                                        Cities[id_origin].name,
+                                        Cities[id_destination].name,
+                                        Cities[id_destination].flag,
+                                        direct_data_value,
+                                        direct_data_date[-2:] + direct_data_date[4:8] + direct_data_date[:4],
+                                        price_data['data'].get(direct_data_date).get('airline'),
+                                        price_data['data'].get(direct_data_date).get('flight_number')
+                                        ).replace(',', ' ')
+
                     bot.send_message(message.chat.id, ticket_info, parse_mode="html", reply_markup=site_button(
                         Cities[id_origin].code,
                         direct_data_date[-2:] + direct_data_date[5:7],
                         Cities[id_destination].code
                     ))
+
+                    first_instance = 0
+                    rows_count = 0
+
+                    for i in Users.select().where(Users.id == message.from_user.id):
+                        rows_count += 1
+                        if rows_count == 1:
+                            first_instance = i
+
+                    if rows_count == 10:
+                        first = Users.delete().where(Users.history_date == first_instance.history_date)
+                        first.execute()
+
+                    user = Users.create(id=message.from_user.id, history_command='/low',
+                                        history_info='{}{} <b>→</b> {}{} Цена: {} руб.\n'.format(
+                                            Cities[id_origin].flag,
+                                            Cities[id_origin].name,
+                                            Cities[id_destination].name,
+                                            Cities[id_destination].flag,
+                                            direct_data_value
+                                        ))
+                    user.save()
                 else:
                     bot.send_message(message.from_user.id, "Что-то пошло не по плану. Повторите попытку позже. "
                                                            "Извините за доставленные неудобства.")
@@ -90,5 +128,6 @@ def get_destination_city(message):
             bot.send_message(message.from_user.id, "Что-то пошло не по плану. Повторите попытку позже. "
                                                    "Извините за доставленные неудобства.")
 
-        bot.send_message(message.from_user.id, "Хотите продолжить?", reply_markup=ask_to_continue_low())
+        bot.set_state(message.from_user.id, StateMemoryStorage(), message.chat.id)
 
+        bot.send_message(message.from_user.id, "Хотите продолжить?", reply_markup=ask_to_continue_low())
