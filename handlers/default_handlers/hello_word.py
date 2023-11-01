cefe341e059ea0fe644a0a06188851750c1d3ed3
GashimovA <agasif15@mail.ru>
2023-11-01 22:39:04 +0300

    added reply to user's text

--- handlers/default_handlers/hello_word.py
+++ handlers/default_handlers/hello_word.py
@@ -1,8 +1,11 @@
 from loader import bot
+from utils import set_bot_commands
 
 
 @bot.message_handler(content_types=['text'])
 def text(message):
     if message.text == 'Привет':
-        bot.reply_to(message, f"Привет, {message.from_user.full_name}! Я бот для поиска подходящих билетов."
-                              f"Выберите команду, чтобы продолжить")
+        bot.reply_to(message, f"Привет, {message.from_user.full_name}! Я бот для поиска подходящих билетов. "
+                              f"Выберите команду, чтобы продолжить.")
+    else:
+        bot.send_message(message.chat.id, f'Выберите команду:\n{set_bot_commands.text}')
