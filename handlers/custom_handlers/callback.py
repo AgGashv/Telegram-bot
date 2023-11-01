70ab3e92af946e621f24a249e243fec0c75ddb8e
GashimovA <agasif15@mail.ru>
2023-11-01 22:37:46 +0300

    added code that removes the "Хотите продолжить?" message after pressing the button

--- handlers/custom_handlers/callback.py
+++ handlers/custom_handlers/callback.py
@@ -9,12 +9,16 @@ from utils import set_bot_commands
 @bot.callback_query_handler(func=lambda call: True)
 def low_callback(call):
     if call.data == 'yes_low':
+        bot.delete_message(call.message.chat.id, call.message.message_id)
         low(call.message)
     elif call.data == 'yes_high':
+        bot.delete_message(call.message.chat.id, call.message.message_id)
         high(call.message)
     elif call.data == 'yes_custom':
+        bot.delete_message(call.message.chat.id, call.message.message_id)
         custom(call.message)
     elif call.data == 'no':
+        bot.delete_message(call.message.chat.id, call.message.message_id)
         bot.set_state(call.message.from_user.id, StateMemoryStorage(), call.message.chat.id)
         bot.send_message(call.message.chat.id, f'Выберите команду:\n{set_bot_commands.text}')
 
