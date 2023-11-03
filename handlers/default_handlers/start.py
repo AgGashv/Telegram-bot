377380f6663cb9059dfba7c6620f354cea4ad615
GashimovA <agasif15@mail.ru>
2023-11-03 11:08:03 +0300

    more information to start command added

--- handlers/default_handlers/start.py
+++ handlers/default_handlers/start.py
@@ -1,9 +1,13 @@
 from telebot.types import Message
 
 from loader import bot
+from config_data.config import DEFAULT_COMMANDS
 
 
 @bot.message_handler(commands=["start"])
 def bot_start(message: Message):
-    bot.reply_to(message, f"Привет, {message.from_user.full_name}! Я бот для поиска подходящих билетов. "
-                          f"Выберите команду, чтобы продолжить")
+    text = f"Привет, {message.from_user.full_name}! Я бот для поиска подходящих билетов. " \
+           f"Выберите команду:\n"
+    for command, desk in DEFAULT_COMMANDS:
+        text += f'/{command} - {desk}\n'
+    bot.reply_to(message, text)
